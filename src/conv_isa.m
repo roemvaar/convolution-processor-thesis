5c95193386c8ce792ebaa72ecd8dc5825583ea0e
Roberto Emmanuel <valenzuelarober@gmail.com>
2019-03-12 22:52:50 -0700

    conv algorithms in matlab

---
+++ src/conv_isa.m
@@ -0,0 +1,18 @@
+function y = conv_isa(x,h)
+    % conv_isa -> convolution operation (input side algorithm)
+    % "x" is the input signal 
+    % "h" is the impulse response
+    % "y" is the output signal 
+    
+    SIZE_X = numel(x);    % number of elements of x 
+    SIZE_H = numel(h);    % number of elements of h
+    
+    y = zeros(1, SIZE_X + SIZE_H - 1);    % zero the output array
+    
+    for i = 1:SIZE_X
+        for j = 1:SIZE_H
+            y(i + j - 1) = y(i + j - 1) + x(i) * h(j);
+        end
+    end    
+end
+
---
+++ src/conv_osa.m
@@ -0,0 +1,10 @@
+function y = conv_osa(x,h)
+    % conv_osa - convolution operation / output side algorithm
+    % "x" is the input signal 
+    % "h" is the impulse response
+    % "y" is the output signal 
+    
+    
+    
+end
+
