a9366a4fa3ff6096477355ed67937f4881f83f4a
Roberto Emmanuel <valenzuelarober@gmail.com>
2019-03-13 12:58:22 -0700

    matlab output side algorithm

--- src/conv_isa.m
+++ src/conv_isa.m
@@ -9,7 +9,7 @@ function y = conv_isa(x,h)
     
     y = zeros(1, SIZE_X + SIZE_H - 1);    % zero the output array
     
-    for i = 1:SIZE_X
+    for i = 1:SIZE_X      % loops through the input signal 
         for j = 1:SIZE_H
             y(i + j - 1) = y(i + j - 1) + x(i) * h(j);
         end
