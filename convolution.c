d5730d1ab67c268d6f14520fa46637bb4417491c
Roberto Valenzuela <valenzuelarober@gmail.com>
2020-12-27 12:44:21 -0700

    Print array function

--- convolution.c
+++ convolution.c
@@ -1,20 +1,20 @@
 // Convolution - One-Dimension and Two-Dimension
 
-
 #include <stdio.h>
 
-#define SIZE_OF_ARRAY
+#define SIZE(A) (sizeof(A)/sizeof(A[0]))
 
 
 void convolution(char *img_ptr, char *kernel);
 void convolution_2d(char *img_ptr, char *kernel);
-
+void print_array(int *array, int size);
 
 int main(void)
 {
     printf("Convolution");
     char *img_8x8[8][8];   // 8x8 Image - Grayscale
-    
+    int array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
     return 0;
 }
 
@@ -37,6 +37,6 @@ void print_array(int *array, int size)
 
     for(i = 0; i < size; i++)
     {
-        printf("%d", 
+        printf("%d", array[i]);
     }
 }
