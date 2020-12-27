9cd2c0ebeab8231f8a2665d92eaeca23f0ee4871
Roberto Valenzuela <valenzuelarober@gmail.com>
2020-12-26 20:25:34 -0600

    8x8 Image

--- convolution.c
+++ convolution.c
@@ -1,23 +1,29 @@
-// Convolution
+// Convolution - One-Dimension and Two-Dimension
 
 
 #include <stdio.h>
 
 
+void convolution(char *img_ptr, char *kernel);
+void convolution_2d(char *img_ptr, char *kernel);
+
+
 int main(void)
 {
     printf("Convolution");
+    char *img_8x8[8][8];   // 8x8 Image
+    
+    return 0;
 }
 
 
-void convolution()
+void convolution(char *img_ptr, char *kernel)
 {
     printf("Convolution");
 }
 
 
-void 2d_convolution()
+void convolution_2d(char *img_ptr, char *kernel)
 {
     printf("2D Convolution");
 }
-
