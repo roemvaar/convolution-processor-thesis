7f974fa6ae8adc5d49a9f64a55e45dcdd97a97c9
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-02-21 11:27:34 -0600

    Update conv.cpp

--- src/c++/conv.cpp
+++ src/c++/conv.cpp
@@ -1,6 +1,33 @@
-// 1D Convolution
+// Convolution
 // 
 // http://www.songho.ca/dsp/convolution/convolution.html
 
+#include <iostream>
+using namespace std;
 
+
+class Convolution
+{
+    public:
+        void conv()
+        {
+            cout << "Convolution 1D" << endl;
+        }
+
+        void conv2d()
+        {
+            cout << "Convolution 2D" << endl;
+        }
+};
+
+
+int main(void)
+{
+    Convolution test_conv;
+
+    test_conv.conv();
+    test_conv.conv2d();
+
+    return 0;
+}
 
