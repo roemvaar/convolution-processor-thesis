6939f8afa70e13fbb5dc221751490b9c937f2081
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-03-03 23:17:52 -0600

    Update proc_elem.v

--- verilog/proc_elem.v
+++ verilog/proc_elem.v
@@ -13,18 +13,24 @@ module proc_elem(clk, x_in, y_in, x_out, y_out, weight);
     input weight;
     output x_out;
     output y_out;
+    
+    wire w_x_in;
+    wire w_y_in;
+    wire w_weight;
+    reg reg_x_out;
+    reg reg_y_out;
 
-    assign reg_x_in = x_in;
-    assign reg_y_in = y_in;
-    assign reg_weight = weight;
+    assign w_x_in = x_in;
+    assign w_y_in = y_in;
+    assign w_weight = weight;
 
     assign x_out = reg_x_out;
     assign y_out = reg_y_out;
   
     always @ (posedge clk)
     begin
-        reg_y_out = reg_y_in * reg_weight * reg_x_in; // MAC operation - yout <- yin + w * xin
-        reg_x_out = reg_x_in; // xout <- xin
+        reg_y_out = w_y_in * w_weight * w_x_in; // MAC operation - yout <- yin + w * xin
+        reg_x_out = w_x_in; // xout <- xin
     end
 
 endmodule
