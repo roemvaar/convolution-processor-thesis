42fac973de9713dcd7165da0e3f654f67f442647
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-06-03 03:01:10 -0500

    Update systolic_array.v

--- verilog/systolic_array.v
+++ verilog/systolic_array.v
@@ -1,6 +1,11 @@
 // Define n processing elements
 
-module systolic_array;
+module systolic_array (x, k, h);
+    input x;
+    input k;
+    output h;
+
+    parameter DATA_WIDTH = 16;
 
 endmodule
-
+ 
